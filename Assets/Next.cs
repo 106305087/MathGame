96f7beea90951ad405991f7ecc5cee563f14eb90
Lily Chou <0329happy0910@gmail.com>
2018-10-20 22:21:14 +0800

    Initial commit

---
+++ Assets/Next.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Next : MonoBehaviour
+{
+
+    public GameObject core;
+    public GameObject option1;
+    public GameObject option2;
+    public GameObject option3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        option1.GetComponent<TextMesh>().color = Color.white;
+        option2.GetComponent<TextMesh>().color = Color.white;
+        option3.GetComponent<TextMesh>().color = Color.white;
+        core.GetComponent<GameController>().GenerateQuestion();
+    }
+}
---
+++ Assets/Option3.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option3 : MonoBehaviour {
+
+    public bool isCorrect;
+    public GameObject core;
+    public GameObject option1;
+    public GameObject option2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        
+
+        if (isCorrect)
+        {
+            GetComponent<TextMesh>().color = Color.green;
+        }
+        else
+        {
+            GetComponent<TextMesh>().color = Color.red;
+            int ans = core.GetComponent<GameController>().option;
+            if (ans == 0)
+            {
+                option1.GetComponent<TextMesh>().color = new Color(255, 151, 0, 255);
+            }
+            else
+            {
+                option2.GetComponent<TextMesh>().color = new Color(255, 151, 0, 255);
+            }
+        }
+    }
+}
